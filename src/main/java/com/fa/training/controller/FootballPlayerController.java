d2682b30a7411071bdbe79df6c9976978da2f3f8
Nguyen Tri Phuong <jason103nguyen@gmail.com>
2021-11-11 20:56:27 +0700

    create method doGetAddFootballPlayer

--- src/main/java/com/fa/training/controller/FootballPlayerController.java
+++ src/main/java/com/fa/training/controller/FootballPlayerController.java
@@ -1,9 +1,13 @@
 package com.fa.training.controller;
 
+import com.fa.training.dto.FootballPlayerDto;
 import com.fa.training.service.FootballPlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 public class FootballPlayerController {
@@ -18,6 +22,12 @@ public class FootballPlayerController {
     @GetMapping(value = "/add-football-player")
     public String doGetAddFootballPlayer() {
 
+        FootballPlayerDto footballPlayerDto = new FootballPlayerDto("Phuong", 1000,
+                LocalDate.parse("11-11-2021", DateTimeFormatter.ofPattern("dd-MM-yyyy")),
+                LocalDate.parse("11-11-2025", DateTimeFormatter.ofPattern("dd-MM-yyyy")),
+                "14");
+
+        footballPlayerService.create(footballPlayerDto);
         return "hello";
     }
 }
--- src/main/java/com/fa/training/dao/AdvertisingContractDaoImpl.java
+++ src/main/java/com/fa/training/dao/AdvertisingContractDaoImpl.java
@@ -2,9 +2,5 @@ package com.fa.training.dao;
 
 import com.fa.training.entities.AdvertisingContract;
 
-public class AdvertisingContractDaoImpl extends AbstractDao<AdvertisingContract> {
-
-    public AdvertisingContractDaoImpl() {
-        super(AdvertisingContract.class);
-    }
+public class AdvertisingContractDaoImpl {
 }
