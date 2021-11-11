0c9a15d5c47cfeb72bcfeae4928bf354b1bf9aa0
Nguyen Tri Phuong <jason103nguyen@gmail.com>
2021-11-11 20:44:05 +0700

    create method add football player at service

--- src/main/java/com/fa/training/controller/FootballPlayerController.java
+++ src/main/java/com/fa/training/controller/FootballPlayerController.java
@@ -1,4 +1,24 @@
 package com.fa.training.controller;
 
+import com.fa.training.service.FootballPlayerService;
+import com.fa.training.service.FootballPlayerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class FootballPlayerController {
+
+    private FootballPlayerServiceImpl footballPlayerServiceImpl;
+
+    @Autowired
+    public FootballPlayerController(FootballPlayerServiceImpl footballPlayerServiceImpl) {
+        this.footballPlayerServiceImpl = footballPlayerServiceImpl;
+    }
+
+    @GetMapping(value = "/add-football-player")
+    public String doGetAddFootballPlayer() {
+
+        return "hello";
+    }
 }
--- src/main/java/com/fa/training/service/FootballPlayerServiceImpl.java
+++ src/main/java/com/fa/training/service/FootballPlayerServiceImpl.java
@@ -1,4 +1,26 @@
 package com.fa.training.service;
 
-public class FootballPlayerServiceImpl {
+import com.fa.training.dao.FootballPlayerDaoImpl;
+import com.fa.training.dto.FootballPlayerDto;
+import com.fa.training.entities.FootballPlayer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FootballPlayerServiceImpl implements FootballPlayerService {
+
+    private FootballPlayerDaoImpl footballPlayerDaoImpl;
+
+    @Autowired
+    public FootballPlayerServiceImpl(FootballPlayerDaoImpl footballPlayerDaoImpl) {
+        this.footballPlayerDaoImpl = footballPlayerDaoImpl;
+    }
+
+    /**
+     * Add a football player to database
+     * @param footballPlayerDto
+     */
+    public void create(FootballPlayerDto footballPlayerDto) {
+        footballPlayerDaoImpl.create(new FootballPlayer(footballPlayerDto));
+    }
 }
