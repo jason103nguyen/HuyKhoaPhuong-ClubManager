98e6fec65a162c963a951f19cadf56c28f93b4e3
Nguyen Tri Phuong <jason103nguyen@gmail.com>
2021-11-11 20:25:18 +0700

    add feature create a footbal player

--- src/main/java/com/fa/training/service/FootballPlayerServiceImpl.java
+++ src/main/java/com/fa/training/service/FootballPlayerServiceImpl.java
@@ -1,4 +1,26 @@
 package com.fa.training.service;
 
+import com.fa.training.dao.FootballPlayerDaoImpl;
+import com.fa.training.dto.FootballPlayerDto;
+import com.fa.training.entities.FootballPlayer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FootballPlayerServiceImpl {
+
+    private FootballPlayerDaoImpl footballPlayerDaoImpl;
+
+    @Autowired
+    public FootballPlayerServiceImpl(FootballPlayerDaoImpl footballPlayerDaoImpl) {
+        this.footballPlayerDaoImpl = footballPlayerDaoImpl;
+    }
+
+    /**
+     * Add a footbal player to database
+     * @param footballPlayerDto
+     */
+    public void create(FootballPlayerDto footballPlayerDto) {
+        footballPlayerDaoImpl.create(new FootballPlayer(footballPlayerDto));
+    }
 }
