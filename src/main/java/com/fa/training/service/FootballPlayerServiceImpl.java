2bccda8c4da47453cea3a8c69e49d4c616aaf39d
Nguyen Tri Phuong <jason103nguyen@gmail.com>
2021-11-11 20:26:35 +0700

    implements interface FootballPlayerService

--- src/main/java/com/fa/training/service/FootballPlayerServiceImpl.java
+++ src/main/java/com/fa/training/service/FootballPlayerServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FootballPlayerServiceImpl {
+public class FootballPlayerServiceImpl implements FootballPlayerService {
 
     private FootballPlayerDaoImpl footballPlayerDaoImpl;
 
