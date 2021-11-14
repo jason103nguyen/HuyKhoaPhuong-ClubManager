2e27e96614d11f2506e9186e9962f90615ce79da
Huy <huyhuy611998@gmail.com>
2021-11-14 21:10:00 +0700

    add DAO of Match and Ticket

--- src/main/java/com/fa/training/dao/MatchDaoImpl.java
+++ src/main/java/com/fa/training/dao/MatchDaoImpl.java
@@ -1,4 +1,18 @@
 package com.fa.training.dao;
 
-public class MatchDaoImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public class MatchDaoImpl extends AbstractDao<Match> {
+
+    @Autowired
+    private LocalSessionFactoryBean localSessionFactoryBean;
+
+    public MatchDaoImpl(LocalSessionFactoryBean localSessionFactoryBean) {
+        super(Match.class, localSessionFactoryBean);
+    }
 }
